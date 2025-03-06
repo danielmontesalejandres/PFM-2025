b7646e4337dd0284f1b65ead19dcf456bd344b5c
danielmontesalejandres <danielmontesalejandres@gmail.com>
2025-03-06 20:00:53 +0100

    añadiendo page

---
+++ src/views/Actores/actores.router.tsx
@@ -0,0 +1,59 @@
+import ActoresPage from '../Actores/index.tsx';
+import { DesktopOutlined } from '@ant-design/icons'
+import { AdminRouterItem } from "../../router";
+//import DemoChart from "./chart";
+//import DemoTable from "./table";
+import { Outlet } from "react-router-dom";
+
+const actoresRouter: AdminRouterItem[] = [
+  {
+    path: 'Actores',
+    element: <ActoresPage />,
+    meta: {
+      label: "Demo",
+      title: "Productor",
+      key: "/Actores",
+      icon: <DesktopOutlined />,
+    },
+    children: [{
+      path: 'chart',
+      element: <ActoresPage />,
+      meta: {
+        label: "chart",
+        title: "chart",
+        key: "/demo/chart",
+        icon: <DesktopOutlined />,
+      }
+    }, {
+      path: 'table',
+      element: <DemoTable />,
+      meta: {
+        label: "table",
+        title: "table",
+        key: "/demo/table",
+        icon: <DesktopOutlined />,
+      }
+    }, {
+      path: 'nested',
+      element: <Outlet />,
+      meta: {
+        label: "nested",
+        title: "nested",
+        key: "/demo/nested",
+        icon: <DesktopOutlined />,
+      },
+      children: [{
+        path: 'table',
+        element: <DemoTable />,
+        meta: {
+          label: "table",
+          title: "table",
+          key: "/demo/nested/table",
+          icon: <DesktopOutlined />,
+        }
+      }]
+    }]
+  },
+]
+
+export default actoresRouter;
